8e3efcf446f57b3c0e3c2b91bfbf05e5521a4049
Jesse O'Brien <jesse@jesse-obrien.ca>
2015-04-14 23:19:48 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"github.com/codegangsta/negroni"
+	"github.com/unrolled/render"
+	"net/http"
+)
+
+var httpPort string
+var httpAddress string
+
+func init() {
+	flag.StringVar(&httpPort, "port", "8080", "The port you wish to bind the server to.")
+	flag.StringVar(&httpAddress, "ip", "127.0.0.1", "The IP address to bind to")
+
+	flag.Parse()
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	r := render.New(render.Options{})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		r.HTML(w, http.StatusOK, "index", nil)
+	})
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(httpAddress + ":" + httpPort)
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# jesseobrien.ca
+
+### Development 
+
+    `bundle install`
+
+#### Sass
+
+    `sass --watch stylesheets/app.scss:public/assets/css/app.css --style compressed`
+
+### Build
+
+    `run build.sh`
+    `commit binary`
+
